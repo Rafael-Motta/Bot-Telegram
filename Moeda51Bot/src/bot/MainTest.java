89bd5efbd80b0aa73f07c8fec2964bf078d4e060
Rafael-Motta <rafael.motta@fatec.sp.gov.br>
2018-04-23 10:44:10 -0300

    Atual

--- Moeda51Bot/src/bot/MainTest.java
+++ Moeda51Bot/src/bot/MainTest.java
@@ -1,19 +1,45 @@
 package bot;
 
+import java.util.List;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ChatAction;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendChatAction;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import com.pengrad.telegrambot.response.SendResponse;
 
 public class MainTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		TelegramBot bot = TelegramBotAdapter.build("571250573:AAHyb-6Ia-Dn80Reluq23nxBrS_yERZ7Pmo");
+		
+		GetUpdatesResponse updatesResponse;
+		SendResponse sendResponse;
+		BaseResponse baseResponse;
+		int msg = 0;
+		
+		Raspa raspa = new Raspa();
+		raspa.raspando();
+		
+		
+		while(true) {
+			updatesResponse = bot.execute(new GetUpdates().limit(100).offset(msg));
+			List<Update> updates = updatesResponse.updates();
+			
+			for(Update update : updates) {
+				msg = update.updateId()+1;
 				
-		Raspa r = new Raspa();
-		
-		r.raspando();
-		
-		System.out.println(r.procurandoPais("austria"));
-		System.out.println(r.procurandoMoeda("dolar"));
-		System.out.println(r.procurandoPadrao("dolar"));
-		
-		
+				baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+				Thread.sleep(500);
+				
+				sendResponse = bot.execute(new SendMessage(update.message().chat().id(), raspa.procurandoPais(update.message().text())));
+				
+			}
+		}
 	}
 }
